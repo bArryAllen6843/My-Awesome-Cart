84b783b74157b60e4c181fe578a9b637bbb8f2af
Aayush Gehlot <aayushgehlot11@gmail.com>
2023-02-22 14:32:02 +0530

    adding some dependencies and some files to deploy the website

--- shop/urls.py
+++ shop/urls.py
@@ -1,5 +1,7 @@
 from . import views
 from django.urls import path
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('', views.index, name="ShopHome"),
@@ -10,4 +12,6 @@ urlpatterns = [
     path('products/<int:myid>', views.productView, name="ProductView"),
     path('checkout/', views.checkout, name="Checkout"),
     path('handlerequest/', views.handlerequest, name="HandleRequest"),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
